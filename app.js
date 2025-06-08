ded9aa1c98719cb50d8b6ddc7a155f4784bdc169
rashii15 <rashindilavanya@gmail.com>
2025-06-08 20:35:46 +0530

    commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let Randomnumber = Math.floor(Math.random()*5)+1;
+console.log(Randomnumber);
+function clickAction() {
+    let getnumber = parseInt(document.getElementById("number").value);
+    
+    if(Randomnumber>getnumber){
+        document.getElementById("result").innerHTML="Try a higher number!! 😕";
+        document.getElementById("image").style.display="none";
+    }
+    if(Randomnumber<getnumber) {
+        document.getElementById("result").innerHTML="Try a lower number!! 😕";
+        document.getElementById("image").style.display="none";
+    }
+    if(Randomnumber==getnumber) {
+        document.getElementById("result").innerHTML="You Won!! 🥳";
+        document.getElementById("image").style.display="block";
+    }
+}
